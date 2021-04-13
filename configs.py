9e7b54d1928526e89c41d34f1fc81232668bcb54
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-04-13 18:55:38 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -11,13 +11,13 @@ class Config(object):
     # Get This From @StringSessionGen_Bot
     STRING_SESSION = os.environ.get("STRING_SESSION")
     # Forward From Chat ID
-    FORWARD_FROM_CHAT_ID = os.environ.get("FORWARD_FROM_CHAT_ID", None)
+    FORWARD_FROM_CHAT_ID = os.environ.get("FORWARD_FROM_CHAT_ID", -1001458807892)
     # Forward To Chat ID
-    FORWARD_TO_CHAT_ID = os.environ.get("FORWARD_TO_CHAT_ID", None)
+    FORWARD_TO_CHAT_ID = os.environ.get("FORWARD_TO_CHAT_ID", -1001283040775)
     # Your User ID
-    USER_ID = os.environ.get("USER_ID", None)
+    USER_ID = os.environ.get("USER_ID", 1406976373)
     # Sleep Time while Kang
-    SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 5))
+    SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 9))
     # Heroku Management
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
